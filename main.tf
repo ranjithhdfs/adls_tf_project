d2470904b6432aa18aa3f8b25f09cbe19fc16891
Ranjith Kumar <ranjithhdfs@gmail.com>
2025-07-04 10:22:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "adls" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled           = true       # Enables ADLS Gen2
+  kind                     = "StorageV2"
+}
